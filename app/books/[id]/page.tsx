b894f0c3a9cc2ce7a92b5d61a732c675b7004115
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2025-07-16 01:33:09 +0500

    fix:errors

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,46 +1,37 @@
-// app/books/[id]/page.tsx
 import books from "@/app/data/book.json";
 import Image from "next/image";
 
-interface Book {
-  id: string | number;
-  title: string;
-  description?: string;
-  image: string;
-}
-
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function BookDetails({ params }: PageProps) {
-  const book: Book | undefined = books.find(
-    (b) => String(b.id) === params.id
-  );
+export default async function BookDetails({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const book = books.find((b) => String(b.id) === params.id);
 
   if (!book) {
     return <div className="p-10 text-red-500">Kitob topilmadi</div>;
   }
 
   return (
-    <div className="container">
-      <div className="flex justify-between items-center gap-4 p-6">
-        <Image
-          src={book.image}
-          alt={book.title}
-          width={500}
-          height={400}
-          className="w-full h-96 object-cover rounded"
-        />
-        <div>
-          <h1 className="text-3xl font-bold mt-6">{book.title}</h1>
-          <p className="mt-4 text-gray-700">
-            {book.description || "Bu kitob haqida hozircha ma'lumot yo'q."}
-          </p>
+    <>
+      <div className="container">
+        <div className="flex justify-between items-center gap-4 p-6">
+          <Image
+            src={book.image}
+            alt={book.title}
+            className="w-full h-96 object-cover rounded"
+            width={500}
+            height={500}
+          />
+          <div>
+            <h1 className="text-3xl font-bold mt-6">{book.title}</h1>
+            <p className="mt-4 text-gray-700">
+              {book.description || "Bu kitob haqida hozircha ma'lumot yo'q."}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
+
