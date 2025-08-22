edbef02c3c70b089a01549aed0b26d3c4f4874b6
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2025-08-23 00:04:21 +0500

    edit error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Created from '@/components/Created/Created'
+// import Created from '@/components/Created/Created'
 import AboutUS from '@/app/about/AboutUS'
 import AboutIntro from '@/app/about/AboutIntro'
 
